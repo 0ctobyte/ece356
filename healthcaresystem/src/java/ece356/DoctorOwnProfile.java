33c1ba36730d079b3ddc265eaff71c1a6a9abfb1
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2015-03-15 15:33:37 -0400

    Update DoctorOwnProfile.java

--- healthcaresystem/src/java/ece356/DoctorOwnProfile.java
+++ healthcaresystem/src/java/ece356/DoctorOwnProfile.java
@@ -10,5 +10,60 @@ package ece356;
  * @author sekharb
  */
 public class DoctorOwnProfile {
+    public enum Gender {
+        M,
+        F
+    }
     
+    private String user_alias;
+    private String name_first;
+    private String name_middle;
+    private String name_last;
+    private Gender gender;
+    private Integer num_years_licensed;
+    private Double avg_rating;
+    private Integer num_reviews;
+    
+    public DoctorOwnProfile(String user_alias, String name_first, String name_middle, String name_last, Gender gender, Integer num_years_licensed, Double avg_rating, Integer num_reviews) {
+        this.user_alias = user_alias;
+        this.name_first = name_first;
+        this.name_middle = name_middle;
+        this.name_last = name_last;
+        this.gender = gender;
+        this.num_years_licensed = num_years_licensed;
+        this.avg_rating = avg_rating;
+        this.num_reviews = num_reviews;
+    }
+    
+    public String getUserAlias() {
+        return this.user_alias;
+    }
+    
+    public String getFirstName() {
+        return this.name_first;
+    }
+    
+    public String getMiddleName() {
+        return this.name_middle;
+    }
+    
+    public String getLastName() {
+        return this.name_last;
+    }
+    
+    public Gender getGender() {
+        return this.gender;
+    }
+    
+    public Integer getNumYearsLicensed() {
+        return this.num_years_licensed;
+    }
+    
+    public Double getAvgRating() {
+        return this.avg_rating;
+    }
+    
+    public Integer getNumReviews() {
+        return this.num_reviews;
+    }
 }
