93f3a4c0633e6bd24cf6eebdc848a928cbe8be97
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2015-03-22 15:19:32 -0400

    Fix performDoctorSearch in DBAO. Update doctor_search_form.jsp

--- healthcaresystem/src/java/ece356/DoctorSearch.java
+++ healthcaresystem/src/java/ece356/DoctorSearch.java
@@ -14,13 +14,15 @@ public class DoctorSearch {
     private String name_first;
     private String name_middle;
     private String name_last;
+    private Integer num_reviews;
     private Double avg_rating;
     
-    public DoctorSearch(String doctor_alias, String name_first, String name_middle, String name_last, Double avg_rating) {
+    public DoctorSearch(String doctor_alias, String name_first, String name_middle, String name_last, Integer num_reviews, Double avg_rating) {
         this.doctor_alias = doctor_alias;
         this.name_first = name_first;
         this.name_middle = name_middle;
         this.name_last = name_last;
+        this.num_reviews = num_reviews;
         this.avg_rating = avg_rating;
     }
     
